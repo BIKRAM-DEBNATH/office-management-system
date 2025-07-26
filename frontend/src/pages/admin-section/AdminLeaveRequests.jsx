3c456888acfe1ecfe5d9b00c406897fc53304822
BIKRAM DEBNATH <bikramdebnath905@gmail.com>
2025-07-26 13:39:13 +0530

    Update AdminLeaveRequests.jsx

--- frontend/src/pages/admin-section/AdminLeaveRequests.jsx
+++ frontend/src/pages/admin-section/AdminLeaveRequests.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./admin-section-css/adminleave.css";
 import axios from "axios";
 import { useAuth } from "../../context/context";
+import "./admin-section-css/scrolling-fix.css";
+
 
 const AdminLeaveRequests = () => {
   const [requests, setRequests] = useState([]);
